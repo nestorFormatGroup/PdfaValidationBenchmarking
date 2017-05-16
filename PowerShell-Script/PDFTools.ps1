b3a45f57496c44ef2a64cbda92bf644e2c1c8311
Claire Röthlisberger <Chlara@users.noreply.github.com>
2017-05-16 17:19:57 +0200

    neue Version und kleine Änderungen

--- PowerShell-Script/PDFTools.ps1
+++ PowerShell-Script/PDFTools.ps1
@@ -6,15 +6,15 @@ Function Show-Duration($start_time)
     $end_time = Get-Date
     $duration = $end_time – $start_time
 
-    ""
-    " PDFTools"
-    "____________________________________________________________"
-    ""
-    " Start      : " + $start_time.DateTime
-    " End        : " + $end_time.DateTime
-    " Duration   : " + $duration.Hours + ":" + $duration.Minutes + ":" + $duration.Seconds
-    "____________________________________________________________"
-    ""
+    "" >> C:\ValidatorenTest\pdftools.txt
+    " PDFTools" >> C:\ValidatorenTest\pdftools.txt
+    "____________________________________________________________" >> C:\ValidatorenTest\pdftools.txt
+    "" >> C:\ValidatorenTest\pdftools.txt
+    " Start      : " + $start_time.DateTime >> C:\ValidatorenTest\pdftools.txt
+    " End        : " + $end_time.DateTime >> C:\ValidatorenTest\pdftools.txt
+    " Duration   : " + $duration.Hours + ":" + $duration.Minutes + ":" + $duration.Seconds >> C:\ValidatorenTest\pdftools.txt
+    "____________________________________________________________" >> C:\ValidatorenTest\pdftools.txt
+    "" >> C:\ValidatorenTest\pdftools.txt
 
 }
 
@@ -27,51 +27,56 @@ Function Run-PDFTools()
     Set-Location -Path C:\Tools
     
     # PDFTools
+    "PDFTools Version:" >> C:\ValidatorenTest\pdftools.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe >> C:\ValidatorenTest\pdftools.txt
+    "" >> C:\ValidatorenTest\pdftools.txt
+
+    # PDFTools
 	"PDFTools: 1a"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1a C:\ValidatorenTest\Testset\1a\*.pdf >> C:\ValidatorenTest\pdftools_1a.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1a C:\ValidatorenTest\Testset\1a\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	# "PDFTools: 1b -> wird unterteilt, damit die Anzahl Dateien weniger problematisch ist"
-    # C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\*.pdf >> C:\ValidatorenTest\pdftools_1b.txt
+    # C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 0"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\0\*.pdf >> C:\ValidatorenTest\pdftools_1b_0.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\0\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 1"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\1\*.pdf >> C:\ValidatorenTest\pdftools_1b_1.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\1\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 2"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\2\*.pdf >> C:\ValidatorenTest\pdftools_1b_2.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\2\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 3"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\3\*.pdf >> C:\ValidatorenTest\pdftools_1b_3.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\3\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 4"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\4\*.pdf >> C:\ValidatorenTest\pdftools_1b_4.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\4\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 5"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\5\*.pdf >> C:\ValidatorenTest\pdftools_1b_5.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\5\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 6"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\6\*.pdf >> C:\ValidatorenTest\pdftools_1b_6.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\6\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 7"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\7\*.pdf >> C:\ValidatorenTest\pdftools_1b_7.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\7\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 8"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\8\*.pdf >> C:\ValidatorenTest\pdftools_1b_8.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\8\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b 9"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\9\*.pdf >> C:\ValidatorenTest\pdftools_1b_9.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\9\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b "
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\A\*.pdf >> C:\ValidatorenTest\pdftools_1b_A.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\A\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b A"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\B\*.pdf >> C:\ValidatorenTest\pdftools_1b_B.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\B\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b B"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\C\*.pdf >> C:\ValidatorenTest\pdftools_1b_C.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\C\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b C"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\D\*.pdf >> C:\ValidatorenTest\pdftools_1b_D.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\D\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b D"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\E\*.pdf >> C:\ValidatorenTest\pdftools_1b_E.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\E\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 1b E"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\F\*.pdf >> C:\ValidatorenTest\pdftools_1b_F.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1b C:\ValidatorenTest\Testset\1b\F\*.pdf >> C:\ValidatorenTest\pdftools.txt
 
     "PDFTools: 2a"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-2a C:\ValidatorenTest\Testset\2a\*.pdf >> C:\ValidatorenTest\pdftools_2a.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-2a C:\ValidatorenTest\Testset\2a\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 2b"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-2b C:\ValidatorenTest\Testset\2b\*.pdf >> C:\ValidatorenTest\pdftools_2b.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-2b C:\ValidatorenTest\Testset\2b\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: 2u"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-2u C:\ValidatorenTest\Testset\2u\*.pdf >> C:\ValidatorenTest\pdftools_2u.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-2u C:\ValidatorenTest\Testset\2u\*.pdf >> C:\ValidatorenTest\pdftools.txt
 	"PDFTools: pdf->1a"
-    C:\Tools\PDF-Tools\VALS480WIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1a C:\ValidatorenTest\Testset\pdf\*.pdf >> C:\ValidatorenTest\pdftools_pdf.txt
+    C:\Tools\PDF-Tools\VALAS4920PreWIN32\bin\pdfvalidator.exe -v -rd -cl pdfa-1a C:\ValidatorenTest\Testset\pdf\*.pdf >> C:\ValidatorenTest\pdftools.txt
   
 	""
 	"Validierung abgeschlossen. "
